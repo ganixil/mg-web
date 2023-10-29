46b9fab002c4b09d44963db2039612b75013c798
ganixil <xiangli1997@gmail.com>
2023-10-29 17:55:14 -0400

    fix build problem

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
-import { Dropdown } from "react-bootstrap";
+//import { Dropdown } from "react-bootstrap";
 import "../styles/Nav.css"
 
 function Nav(){

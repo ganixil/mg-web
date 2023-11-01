469fe0cbd25bc41afd0fa8d44e4a280d200f0b41
ganixil <xiangli1997@gmail.com>
2023-11-01 18:55:20 -0400

    adding news format

--- src/components/News.js
+++ src/components/News.js
@@ -3,13 +3,21 @@ import data from '../data/news';
 import '../styles/News.css';
 function News(){
     //const maxArticle = 10;
+
+
     return(
         <div className="articleContainer">
             <h1>Coop News</h1>
             {data.map((article) => 
                 <Link to='/news/article' state={{ title: article.title, content: article.content, date: article.date}}>
-                    <h2>{article.title} - {article.date}</h2>
-                    <p>{article.content.slice(0,50)} ...</p>
+                    <div className='news-title'>
+                        <h2>{article.title}</h2>
+                        {article.weight == "new" &&
+                        <h2 className='tag' >New!!</h2>
+                        }
+                    </div> 
+                    <p>{article.date}</p>
+                    <p>{article.content.slice(3,50)} ...</p>
                 </Link>
             )}  
         </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,14 @@
 import { useLocation } from "react-router-dom";
+import parse from 'html-react-parser';
+import '../styles/ArticlePage.css'
 function ArticlePage(){
     const location = useLocation()
     const { title, content,date} = location.state;
     return (
         <>
-            <h1>{title}</h1>
-            <h3>{date}</h3>
-            <p>{content}</p>
+            <h1 className="title">{title}</h1>
+            <h3 className="date">{date}</h3>
+            <div className="content">{parse(content)}</div>
         </>
     );
 }

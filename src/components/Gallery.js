9b6395fee6bb777fa6d174e96431ced504f47fa0
ganixil <xiangli1997@gmail.com>
2023-11-01 18:09:42 -0400

    adding new carousel images and logo

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,36 @@
 import '../styles/Gallery.css';
 import ImageGallery from "react-image-gallery";
+import carousel1 from '../assets/images/carousel/carousel1.jpg';
+import carousel2 from '../assets/images/carousel/carousel2.jpg';
+import carousel3 from '../assets/images/carousel/carousel3.jpg';
+import carousel4 from '../assets/images/carousel/carousel4.jpg';
+import carousel5 from '../assets/images/carousel/carousel5.jpg';
+import largelogo from '../assets/images/logo_large.png';
 function Gallery(){
     const images = [
         {
-          original: "https://picsum.photos/id/1018/1000/600/",
-          thumbnail: "https://picsum.photos/id/1018/250/150/",
+          original: largelogo,
+          thumbnail: largelogo,
         },
         {
-          original: "https://picsum.photos/id/1015/1000/600/",
-          thumbnail: "https://picsum.photos/id/1015/250/150/",
+          original: carousel1,
+          thumbnail: carousel1,
         },
         {
-          original: "https://picsum.photos/id/1019/1000/600/",
-          thumbnail: "https://picsum.photos/id/1019/250/150/",
+          original: carousel2,
+          thumbnail: carousel2,
+        },
+        {
+          original: carousel3,
+          thumbnail: carousel3,
+        },
+        {
+          original: carousel4,
+          thumbnail: carousel4,
+        },
+        {
+          original: carousel5,
+          thumbnail: carousel5,
         },
     ];
     return(

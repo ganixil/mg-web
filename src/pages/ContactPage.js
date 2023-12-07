38622702c4015e01ff1b81787385b3b5682e9a98
ganixil <xiangli1997@gmail.com>
2023-12-07 17:01:24 -0500

    Adding form submit service

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import '../styles/pages/ContactPage.css'
 function ContactPage(){
     const SERVICE_ID = process.env.REACT_APP_EMAIL_ID;
     const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
@@ -8,8 +9,6 @@ function ContactPage(){
     const form = useRef();
 
     const sendEmail = (e) => {
-      e.preventDefault();
-  
       emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
         .then((result) => {
             console.log(result.text);
@@ -30,23 +29,33 @@ function ContactPage(){
     }
   
     return (
-      <form ref={form} onSubmit={sendEmail}>
+      <form className="contactus_form" ref={form} onSubmit={sendEmail}>
+        <h1>Contact Us</h1>
         <label>Topic</label>
-        <select name="topic">
+        <select name="topic" required>
+            <option disabled selected value=""> -- Select a topic -- </option>
             <option value="Contact">Contact</option>
             <option value="Grievance">Grievance</option>
             <option value="Other">Other</option>
         </select>
+
         <label>Name</label>
-        <input type="text" name="from_name" />
+        <input type="text" name="from_name" placeholder='your name ...' required/>
+
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" placeholder='your email ...'/>
+
+        <label>Phone Number</label>
+        <input type="tel" name="phone" placeholder='your phone number ...'/>
+
         <label>Message</label>
-        <textarea name="message" />
+        <textarea name="message" placeholder='your message ...' required/>
+
         <label>Apartment #</label>
-        <select name="apartment">
+        <select name="apartment" required>
+            <option disabled selected value=""> -- Select your apartment number -- </option>
             {retriveApartmentNumbers().map((apt) =>
-                <option value={apt}>{apt}</option>
+                <option value="{{apt}}">{apt}</option>
             )}
         </select>
         <input type="submit" value="Send" />

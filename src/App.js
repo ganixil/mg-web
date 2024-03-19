71b9ffc0276a0f565fe0ae8d0231118001c1d8ea
ganixil <xiangli1997@gmail.com>
2024-03-19 18:14:11 -0400

    adding a scroll to top function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './components/Nav';
 import Routing from './pages/Routing';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='AppContainer'>
         <Nav />
+        <ScrollToTop />
         <Routing />
         <Footer/>
       </div>

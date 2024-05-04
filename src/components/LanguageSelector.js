1ebac51d633012aaa5ad68a3eac87dee69949fa6
ganixil <xiangli1997@gmail.com>
2024-05-04 11:48:08 -0400

    adding some spanish support

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -12,6 +12,7 @@ function LanguageSelector()
             <option disabled selected value="">{t('language-selector.label')}</option>
             <option value="en">{t('language-selector.languages.en')}</option>
             <option value="zh">{t('language-selector.languages.zh')}</option>
+            <option value="es">{t('language-selector.languages.es')}</option>
         </select>     
     </div>
 }
